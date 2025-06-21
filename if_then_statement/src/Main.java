b31dbd235e8943344089bda7a0eb6a200a31ea64
AtiBexx2 <att.benko@gmail.com>
2025-06-21 03:52:46 +0200

    Add new files and folders

---
+++ if_then_statement/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+        } else {
+            System.out.println("It is an alien!");
+        }
+    }
+}
